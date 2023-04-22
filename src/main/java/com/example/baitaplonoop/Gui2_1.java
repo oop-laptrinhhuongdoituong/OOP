36765d793b6d3925fee099d07d0dac07f8862d06
Hung <Hung.HD210399@sis.hust.edu.vn>
2023-04-22 23:09:34 +0700

    chỉnh code

--- src/main/java/com/example/baitaplonoop/Gui2_1.java
+++ src/main/java/com/example/baitaplonoop/Gui2_1.java
@@ -4,17 +4,26 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Gui2_1 implements Initializable {
+    @FXML
+    AnchorPane gui2_1;
     @FXML
     private TableView<addQuestion> table;
 
@@ -23,90 +32,238 @@ public class Gui2_1 implements Initializable {
 
     @FXML
     private TableColumn<addQuestion, Button> action;
-    private ObservableList<addQuestion> questionsList= FXCollections.observableArrayList();;
+    private ObservableList<addQuestion> questionsList = FXCollections.observableArrayList();
+    ;
     @FXML
-   private Label Default;
+    private Label Default;
     @FXML
     private TreeView<String> category;
-    DBConnect db=new DBConnect();
-    void insertCategory(String query,TreeItem<String> root) throws Exception {
+    @FXML
+    private CheckBox gui2_1CheckBox;
+
+    DBConnect db = new DBConnect();
+
+    void insertCategory(String query, TreeItem<String> root) throws Exception {
         table.setVisible(false);
-      ResultSet rs1=db.getData(query);
-      try {
-          while(rs1.next()){
-              String childrenCategory=rs1.getString("categoryName");
-              TreeItem<String> item= new TreeItem<>(childrenCategory);
-              String a="Select * from dbo.Category where parentID = N'"+rs1.getString("categoryID")+"'";
-               insertCategory(a,item);
-               root.getChildren().add(item);
+        ResultSet rs1 = db.getData(query);
+        try {
+            while (rs1.next()) {
+                String childrenCategory = rs1.getString("categoryName");
+                TreeItem<String> item = new TreeItem<>(childrenCategory);
+                String a = "Select * from dbo.Category where parentID = N'" + rs1.getString("categoryID") + "'";
+                insertCategory(a, item);
+                root.getChildren().add(item);
 
-          }
+            }
 
-      }catch (Exception e){
-          throw new Exception(e);
-      }
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
     }
-   @Override
-    public void initialize(URL location, ResourceBundle resourceBundle){
+    public void insertQuestionIntoTableViewWithSubcategory(TreeItem<String> item){
+        String questionView="select *from dbo.Question as q,dbo.Category as c where q.categoryID = c.categoryID and c.categoryName = N'"+item.getValue()+"'";
+        ResultSet rs1=db.getData(questionView);
+        try {
+            while (rs1.next()){
+                addQuestion question1=new addQuestion(rs1.getString("questionID"),rs1.getString("questionText"),new Button("Edit"));
+                questionsList.add(question1);
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        if(item.isLeaf()==false){
+            for(TreeItem<String> subItem : item.getChildren()){
+                insertQuestionIntoTableViewWithSubcategory(subItem);
+            }
+        }
+    }
+    private boolean isMouseOnLabel(Label label, MouseEvent event) {
+        Bounds bounds = label.getBoundsInParent();
+        double mouseX = event.getSceneX();
+        double mouseY = event.getSceneY();
+        return bounds.contains(mouseX, mouseY);
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resourceBundle) {
         category.setVisible(false);
         table.setVisible(false);
+
         Default.setOnMouseClicked(mouseEvent -> {
 
-            TreeItem<String> root=new TreeItem<>("For IT");
+            TreeItem<String> root = new TreeItem<>("For IT");
             try {
-                insertCategory("select * from dbo.Category where parentID is NULL",root);
+                insertCategory("select * from dbo.Category where parentID is NULL", root);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
             category.setRoot(root);
             category.setVisible(true);
         });
-       category.setOnMouseClicked(mouseEvent -> {
-           questionsList.clear();
-           table.setItems(questionsList);
-         if(mouseEvent.getClickCount()==2 && category.getSelectionModel().getSelectedItem().isLeaf()==true){
+        gui2_1CheckBox.setSelected(false);
+        if (gui2_1CheckBox.isSelected()) {
+            category.setOnMouseClicked(mouseEvent -> {
+                questionsList.clear();
+                table.setItems(questionsList);
+                if(mouseEvent.getClickCount()==3){
 
-             TreeItem<String> item=category.getSelectionModel().getSelectedItem();
-             category.setVisible(false);
-             Default.setText(item.getValue());
-             String questionView="select *from dbo.Question as q,dbo.Category as c where q.categoryID = c.categoryID and c.categoryName = N'"+item.getValue()+"'";
-             ResultSet rs1=db.getData(questionView);
-             try {
-                 while (rs1.next()){
-                         addQuestion question1=new addQuestion(rs1.getString("questionID"),rs1.getString("questionText"),new Button("Edit"));
-                         questionsList.add(question1);
-                 }
-                 question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
-                 action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
-                 table.setItems(questionsList);
-                 table.setVisible(true);
-             }catch (Exception e){
-                 throw new RuntimeException(e);
-             }
-         }
-       });
-       category.setOnKeyPressed(keyEvent -> {
-           if(keyEvent.getCode()== KeyCode.ENTER &&category.getSelectionModel().getSelectedItem().isLeaf()==true){
-               questionsList.clear();
-               table.setItems(questionsList);
-               TreeItem<String> item=category.getSelectionModel().getSelectedItem();
-               category.setVisible(false);
-               String questionView="select *from dbo.Question as q,dbo.Category as c where q.categoryID = c.categoryID and c.categoryName = N'"+item.getValue()+"'";
-               ResultSet rs1=db.getData(questionView);
-               Default.setText(item.getValue());
-               try {
-                   while (rs1.next()){
-                       addQuestion question1=new addQuestion(rs1.getString("questionID"),rs1.getString("questionText"),new Button("Edit"));
-                       questionsList.add(question1);
-                   }
-                   question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
-                   action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
-                   table.setItems(questionsList);
-                   table.setVisible(true);
-               }catch (Exception e){
-                   throw new RuntimeException(e);
-               }
-           }
-       });
-   }
+                    TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                    insertQuestionIntoTableViewWithSubcategory(item);
+                    category.setVisible(false);
+                    Default.setText(item.getValue());
+                    question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                    action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                    table.setItems(questionsList);
+                    table.setVisible(true);
+                }
+            });
+            category.setOnKeyPressed(keyEvent -> {
+                questionsList.clear();
+                table.setItems(questionsList);
+                if(keyEvent.getCode()== KeyCode.ENTER ){
+                    TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                    insertQuestionIntoTableViewWithSubcategory(item);
+                    category.setVisible(false);
+                    Default.setText(item.getValue());
+                    question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                    action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                    table.setItems(questionsList);
+                    table.setVisible(true);
+                }
+            });
+        }
+        else {
+            category.setOnMouseClicked(mouseEvent -> {
+                questionsList.clear();
+                table.setItems(questionsList);
+                if(mouseEvent.getClickCount()==3){
+
+                    TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                    category.setVisible(false);
+                    Default.setText(item.getValue());
+                    String questionView="select *from dbo.Question as q,dbo.Category as c where q.categoryID = c.categoryID and c.categoryName = N'"+item.getValue()+"'";
+                    ResultSet rs1=db.getData(questionView);
+                    try {
+                        while (rs1.next()){
+                            addQuestion question1=new addQuestion(rs1.getString("questionID"),rs1.getString("questionText"),new Button("Edit"));
+                            questionsList.add(question1);
+                        }
+                        question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                        action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                        table.setItems(questionsList);
+                        table.setVisible(true);
+                    }catch (Exception e){
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            category.setOnKeyPressed(keyEvent -> {
+                if(keyEvent.getCode()== KeyCode.ENTER){
+                    questionsList.clear();
+                    table.setItems(questionsList);
+                    TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                    category.setVisible(false);
+                    String questionView="select *from dbo.Question as q,dbo.Category as c where q.categoryID = c.categoryID and c.categoryName = N'"+item.getValue()+"'";
+                    ResultSet rs1=db.getData(questionView);
+                    Default.setText(item.getValue());
+                    try {
+                        while (rs1.next()){
+                            addQuestion question1=new addQuestion(rs1.getString("questionID"),rs1.getString("questionText"),new Button("Edit"));
+                            questionsList.add(question1);
+                        }
+                        question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                        action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                        table.setItems(questionsList);
+                        table.setVisible(true);
+                    }catch (Exception e){
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }
+        gui2_1CheckBox.setOnAction(event -> {
+            if (gui2_1CheckBox.isSelected()) {
+                category.setOnMouseClicked(mouseEvent -> {
+                    questionsList.clear();
+                    table.setItems(questionsList);
+                    if(mouseEvent.getClickCount()==3){
+
+                        TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                        insertQuestionIntoTableViewWithSubcategory(item);
+                        category.setVisible(false);
+                        Default.setText(item.getValue());
+                        question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                        action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                        table.setItems(questionsList);
+                        table.setVisible(true);
+                    }
+                });
+                category.setOnKeyPressed(keyEvent -> {
+                    questionsList.clear();
+                    table.setItems(questionsList);
+                    if(keyEvent.getCode()== KeyCode.ENTER ){
+                        TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                        insertQuestionIntoTableViewWithSubcategory(item);
+                        category.setVisible(false);
+                        Default.setText(item.getValue());
+                        question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                        action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                        table.setItems(questionsList);
+                        table.setVisible(true);
+                    }
+                });
+            }
+            else {
+                category.setOnMouseClicked(mouseEvent -> {
+                    questionsList.clear();
+                    table.setItems(questionsList);
+                    if(mouseEvent.getClickCount()==3){
+
+                        TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                        category.setVisible(false);
+                        Default.setText(item.getValue());
+                        String questionView="select *from dbo.Question as q,dbo.Category as c where q.categoryID = c.categoryID and c.categoryName = N'"+item.getValue()+"'";
+                        ResultSet rs1=db.getData(questionView);
+                        try {
+                            while (rs1.next()){
+                                addQuestion question1=new addQuestion(rs1.getString("questionID"),rs1.getString("questionText"),new Button("Edit"));
+                                questionsList.add(question1);
+                            }
+                            question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                            action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                            table.setItems(questionsList);
+                            table.setVisible(true);
+                        }catch (Exception e){
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+                category.setOnKeyPressed(keyEvent -> {
+                    if(keyEvent.getCode()== KeyCode.ENTER){
+                        questionsList.clear();
+                        table.setItems(questionsList);
+                        TreeItem<String> item=category.getSelectionModel().getSelectedItem();
+                        category.setVisible(false);
+                        String questionView="select *from dbo.Question as q,dbo.Category as c where q.categoryID = c.categoryID and c.categoryName = N'"+item.getValue()+"'";
+                        ResultSet rs1=db.getData(questionView);
+                        Default.setText(item.getValue());
+                        try {
+                            while (rs1.next()){
+                                addQuestion question1=new addQuestion(rs1.getString("questionID"),rs1.getString("questionText"),new Button("Edit"));
+                                questionsList.add(question1);
+                            }
+                            question.setCellValueFactory(new PropertyValueFactory<addQuestion,String>("questionText"));
+                            action.setCellValueFactory(new PropertyValueFactory<addQuestion,Button>("button"));
+                            table.setItems(questionsList);
+                            table.setVisible(true);
+                        }catch (Exception e){
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+            }
+        });
+        gui2_1.setOnMouseClicked(mouseEvent -> {
+            if(category.isVisible()&& isMouseOnLabel(Default,mouseEvent)==false)category.setVisible(false);
+        });
+    }
 }
---
+++ src/main/java/com/example/baitaplonoop/Gui6_5.java
@@ -0,0 +1,3 @@
+package com.example.baitaplonoop;
+public class Gui6_5 {
+}
