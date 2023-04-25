a9ba5277ff5934e5173aa91534d59671b1cfbc47
Phuc <hoangphucythus@gmail.com>
2023-04-25 10:34:45 +0700

    add Main

---
+++ src/main/java/com/example/baitaplonoop/Main.java
@@ -0,0 +1,25 @@
+package com.example.baitaplonoop;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    FXMLLoader loader = new FXMLLoader(Main.class.getResource("GUI11.fxml"));
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
