77d28d7792ec37fc90ca4ef64bf1e630c6015950
your name <truongdat.work@gmail.com>
2023-04-27 10:45:02 +0700

    add controller

---
+++ src/main/java/com/example/baitaplonoop/controller/GUI33Controller.java
@@ -0,0 +1,25 @@
+package com.example.baitaplonoop.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class GUI33Controller implements Initializable {
+    public ComboBox parentCategory_cb;
+    public TextField nameCategory_tf;
+    public TextArea infoCategory_ta;
+    public TextField categoryID_tf;
+    public Button addCategory_Btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/com/example/baitaplonoop/controller/GUI35Controller.java
@@ -0,0 +1,13 @@
+package com.example.baitaplonoop.controller;
+
+import javafx.scene.control.*;
+
+public class GUI35Controller {
+    public TextField quizName_tf;
+    public TextArea quizDescription_tf;
+    public DatePicker openQuiz_dp;
+    public DatePicker closeQuiz_dp;
+    public TextField timeQuiz_tf;
+    public ComboBox expires_cb;
+    public Button createQuiz_btn;
+}
