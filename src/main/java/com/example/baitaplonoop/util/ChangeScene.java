4d134bb7957314fb9593a4719df2078f8d6dffd3
Phuc <hoangphucythus@gmail.com>
2023-05-30 06:55:32 +0700

    Phuc

--- src/main/java/com/example/baitaplonoop/util/ChangeScene.java
+++ src/main/java/com/example/baitaplonoop/util/ChangeScene.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
